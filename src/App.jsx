2cfc80fe49706be3278acf88c521c23034b798db
Rob Graff <rgraff@everspringpartners.com>
2026-05-10 09:54:24 -0500

    move header into component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,4 @@
-function Header() {
-  return (
-    // Wrap multiline components in parentheses. DON'T forget the return.
-    <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-      <h1>React Essentials!</h1>
-      <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
+import Header from "./components/header";
 
 function App() {
   return (
