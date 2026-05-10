665bd6092c0825239e343da3327337e1c7b66dcc
Rob Graff <rgraff@everspringpartners.com>
2026-05-10 12:13:49 -0500

    speard operator, et al

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
 import Header from "./components/header";
+import { CORE_CONCEPTS } from "./data"; // use curly braces for a named export
+
+// function CoreConcept(props) {
+//   return (
+//     <li>
+//       <img src={props.image} alt={props.title} />
+//       <h3>{props.title}</h3>
+//       <p>{props.description}</p>
+//     </li>
+//   )
+// }
+
+function CoreConcept({image, title, description}) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  )
+}
 
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]}/>
+            <CoreConcept {...CORE_CONCEPTS[1]}/>
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/>
+          </ul>
+        </section>
       </main>
     </div>
   );
